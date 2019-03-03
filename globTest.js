4aa3e96332dc67dd40861593da588b34cb12bbb4
winton <mail@wintoni.us>
2019-03-02 21:06:32 -0500

    remove dot.reset()

--- globTest.js
+++ globTest.js
@@ -1,10 +1,10 @@
 /* eslint-env jest */
 
-var dot = require("dot-event")(),
+var dot,
   glob = require("./glob")
 
 beforeEach(function() {
-  dot.reset()
+  dot = require("dot-event")()
   glob(dot)
 })
 
